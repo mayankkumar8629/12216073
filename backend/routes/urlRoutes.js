7d258efa1f4aa2a6539e16365d468417e21146ae
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-07-14 12:07:42 +0530

    commit

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { urlShortner,redirectToOrginalUrl } from "../controllers/urlController.js";
+import {logger} from "../../logging/index.js";
 
 const router=express.Router();
 
-router.post('/api/shorten', urlShortner);
-router.get('/:shortCode', redirectToOrginalUrl);
+router.post('/api/shorten',logger, urlShortner);
+router.get('/:shortCode',logger, redirectToOrginalUrl);
 
 export default router;
